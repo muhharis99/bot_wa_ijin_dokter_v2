80c5e48f85cfe0706b88f9da1396373aff8f4b62
Muhammad Haris <61509864+muhharis99@users.noreply.github.com>
2026-08-27 09:44:00 +0700

    feat: enable back to top on master page

--- master.php
+++ master.php
@@ -316,6 +316,7 @@ $schedules = $pdo->query("
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script src="https://npmcdn.com/flatpickr/dist/l10n/id.js"></script>
+    <script src="assets/back-to-top.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             const language = {search: 'Cari:', lengthMenu: 'Tampilkan _MENU_ data', info: 'Menampilkan _START_ sampai _END_ dari _TOTAL_ data', infoEmpty: 'Tidak ada data', zeroRecords: 'Data tidak ditemukan', emptyTable: 'Belum ada data', paginate: {first: 'Awal', last: 'Akhir', next: 'Berikutnya', previous: 'Sebelumnya'}};
